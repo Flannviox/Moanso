c5e284162f66e3960daf15d02f0b74c12e843c05
Belen <anabelensanchezboy@gmail.com>
2024-09-29 00:03:28 -0500

    Mostrar el resultado xd

--- funciones.py
+++ funciones.py
@@ -6,3 +6,5 @@ num1 = float(input("Ingresa el primer número: "))
 num2 = float(input("Ingresa el segundo número: "))
 
 resultado = restar(num1, num2)
+
+print(f"La resta de {num1} menos {num2} es: {resultado}")
