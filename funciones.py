0e764b91c273d0c05e2cbceda8387c5d30918f02
Flavio Zavaleta <zflavioalberto@gmail.com>
2024-09-28 23:32:41 -0500

    sumar listas

--- funciones.py
+++ funciones.py
@@ -1,1 +1,18 @@
-print("Calculadora xd")
+def sumar(lista):
+    total = 0
+    for i in lista:
+        total += i
+
+    return total
+
+
+print("Ingresa la cantidad de numeros que quieres sumar")
+cantidad = int(input())
+lista=[]
+for i in range(cantidad):
+    print("ingresa el valor numero ", i+1)
+    num = int(input())  
+    lista.append(num)
+
+print(lista)
+print("La suma total es: ", sumar(lista))
