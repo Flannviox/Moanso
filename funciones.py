a62107615711861bd34d7d978c10b0da788a0718
YuleisyQuipuzcoa22 <yuleisyquipuzcoa@gmail.com>
2024-09-28 23:39:34 -0500

    interfaz usuario restar

--- funciones.py
+++ funciones.py
@@ -1,1 +1,5 @@
-print ("holi bell")
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+resultado = restar(num1, num2)
+print(f"La resta de {num1} menos {num2} es: {resultado}")
