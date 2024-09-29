5578c177d5f344fc64419243daccd69aa1b3350d
PapuLap <bruce0126alfah@gmail.com>
2024-09-29 00:14:26 -0500

    change two

--- funciones.py
+++ funciones.py
@@ -4,3 +4,9 @@ def multiplicar(x,y):
     a = x*y
     return print(f"La multiplicación de {x} y {y} es {a}")
 
+def potencia():
+    base = float(input("ingrese la base: "))
+    exponente = float(input("Ingrese el exponente: "))
+    resultado = base ** exponente
+    print (f"{base} elevado a {exponente} es:  {resultado}")
+
