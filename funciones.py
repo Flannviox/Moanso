3b098811cf76d3491314e1ed985726af22820aa5
Levi360xd <163799424+Levi360xd@users.noreply.github.com>
2024-09-28 23:21:01 -0500

    Creacion de la funcion potencia

--- funciones.py
+++ funciones.py
@@ -1,1 +1,8 @@
-print("Calculadora xd")
+
+def potencia():
+    base = float(input("ingrese la base: "))
+    exponente = float(input("Ingrese el exponente: "))
+    resultado = base ** exponente
+    print (f"{base} elevado a {exponente} es:  {resultado}")
+
+potencia()
